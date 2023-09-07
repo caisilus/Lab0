9c9ea53e28498e55067b425090d8797be919b919
caisilus <serangor@gmail.com>
2023-09-07 12:45:29 +0300

    Add integration

--- src/index.js
+++ src/index.js
@@ -16,3 +16,15 @@ alert("Let's integrate!")
 a = get_input("Enter left integration limit")
 b = get_input("Enter right integration limit")
 console.log(`a=${a}, b=${b}`)
+
+function integrate(a, b, f, n=1000.0) {
+    deltaX = (b - a) / n
+    sum = 0
+    for (let i = 0; i < n; i++) {
+        sum += f(a + deltaX * i)
+    }
+    return sum * deltaX
+}
+
+f = (x) => x*x - Math.cos(x)
+alert(`Result = ${integrate(a, b, f)}`)
