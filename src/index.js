3e1aeb12c2f76cead970af7b870ce799353248db
caisilus <serangor@gmail.com>
2023-09-07 12:36:16 +0300

    User input

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
+function get_input(prompt_message) {
+    input = prompt(prompt_message)
+    flag = false
+    do {
+        value = parseFloat(input)
+        if (!isNaN(value)) {
+            break;
+        }
+        input = prompt("Error. Please enter again")
+    } while(true)
+
+    return value
+}
+
 alert("Let's integrate!")
+a = get_input("Enter left integration limit")
+b = get_input("Enter right integration limit")
+console.log(`a=${a}, b=${b}`)
